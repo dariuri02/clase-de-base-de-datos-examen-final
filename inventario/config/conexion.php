a091b7670f66278768f949dde1acb201ab44f6bf
dariuri02 <dariuri02@gmail.com>
2026-05-21 09:59:59 -0500

    Add files via upload

---
+++ inventario/config/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$contrasena = "";
+$bd = "inventario_db";
+
+$conn = new mysqli($host, $usuario, $contrasena, $bd);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+?>
+
